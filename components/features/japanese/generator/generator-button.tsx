8cfd87e519b95646ab4034ad9b8bac31186e4e85
Thomas <48889873+ThoDon@users.noreply.github.com>
2025-04-26 17:27:11 +0200

    fix pdf generation in ios

--- components/features/japanese/generator/generator-button.tsx
+++ components/features/japanese/generator/generator-button.tsx
@@ -6,30 +6,6 @@ import { Button } from "@/components/ui/button";
 import { Info, Loader, Printer } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
-async function generatePdf(
-  exercises: Exercise[],
-  showCorrection: boolean,
-  categories: SyllabarySubset[],
-  dictionary: Record<string, string>
-) {
-  const response = await fetch("/api/generate-pdf", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ exercises, showCorrection, dictionary, categories }),
-  });
-
-  if (!response.ok) {
-    throw new Error("Failed to generate PDF");
-  }
-
-  const blob = await response.blob();
-
-  // Open PDF in new tab
-  const url = URL.createObjectURL(blob);
-  window.open(url, "_blank");
-  URL.revokeObjectURL(url); // Clean up the URL object
-}
-
 type GeneratorButtonProps = {
   exercises: Exercise[];
   showCorrection: boolean;
@@ -83,4 +59,35 @@ export function GeneratorButton(props: GeneratorButtonProps) {
       )}
     </>
   );
+}
+
+async function generatePdf(
+  exercises: Exercise[],
+  showCorrection: boolean,
+  categories: SyllabarySubset[],
+  dictionary: Record<string, string>
+) {
+  const response = await fetch("/api/generate-pdf", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ exercises, showCorrection, dictionary, categories }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to generate PDF");
+  }
+
+  const blob = await response.blob();
+  const url = URL.createObjectURL(blob);
+
+  // Create temporary link and trigger download
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "kana-sheet-exercises.pdf"; // Set filename
+  document.body.appendChild(link);
+  link.click();
+
+  // Clean up
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
 }
